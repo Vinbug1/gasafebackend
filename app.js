5c4af775829bf804eebb639d6f8ffe171d9ae8ac
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2023-07-02 11:26:15 +0100

    fixing things out

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
   });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
