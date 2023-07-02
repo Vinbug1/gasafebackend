4c48acd780aa0bfd74f800506495a4b28585499d
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2023-07-02 09:08:15 +0100

    fixing image storage by Vincent

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,6 @@ const FILE_TYPE_MAP = {
   "image/jpeg": "jpeg",
   "image/gif": "gif",
 };
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const isValid = FILE_TYPE_MAP[file.mimetype];
@@ -25,9 +24,29 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(" ").join("-");
     const extension = FILE_TYPE_MAP[file.mimetype];
-    cb(null, `${fileName}-${Date.now()}.${extension}`);
+    const timestamp = Date.now();
+    cb(null, `${fileName}-${timestamp}.${extension}`);
   },
 });
+
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     const isValid = FILE_TYPE_MAP[file.mimetype];
+//     let uploadError = new Error("Invalid image type");
+
+//     if (isValid) {
+//       uploadError = null;
+//     }
+
+//     cb(uploadError, "public/uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     const fileName = file.originalname.split(" ").join("-");
+//     const extension = FILE_TYPE_MAP[file.mimetype];
+//     cb(null, `${fileName}-${Date.now()}.${extension}`);
+//   },
+// });
 
 const uploadOptions = multer({ storage: storage });
 
